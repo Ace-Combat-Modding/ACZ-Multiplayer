f95e94bfb222024b017dcc344cf9c58167a83692
Andrei <46579508+Andreisgl@users.noreply.github.com>
2025-11-25 00:25:32 -0300

    feat: Implement data retrieval and broadcast

--- client.py
+++ client.py
@@ -2,9 +2,14 @@ from twisted.internet import reactor, protocol, task
 import json
 import time
 
-TICK_RATE = 1  # ticks per second
+import game_latch as gl
+
+TICK_RATE = 5  # ticks per second
 SERVER_HOST = "127.0.0.1"
 SERVER_PORT = 1234
+
+GAME_PROCESS_NAME = "pcsx2.exe"
+LATCH = gl.GameLatch(process_name=GAME_PROCESS_NAME)
 
 class GameClient(protocol.Protocol):
     def connectionMade(self):
@@ -17,6 +22,7 @@ class GameClient(protocol.Protocol):
         # Example initial message
         #self.transport.write(b"HELLO_SERVER")
         #print("sending: HELLO_SERVER")
+
 
     def dataReceived(self, data):
         ''' Receive data from server. Called automatically by Twisted. '''
@@ -32,12 +38,17 @@ class GameClient(protocol.Protocol):
         This runs at your TICK_RATE.
         Poll your game state here and send packets if needed.
         '''
+
+        player_data = LATCH.get_player_data('altitude')
+
+        #print(f'Player Altitude: {}')
+
         # Example: send a JSON state packet
         packet = {
-            "timestamp": time.time(),
-            "px": 12.3,  # example position
-            "py": -4.1,
-            "pz": 0.5,
+            #"timestamp": time.time(),
+            "px": LATCH.get_player_data('pos_east_west'),
+            "py": LATCH.get_player_data('pos_north_south'),
+            "altitude": LATCH.get_player_data('altitude'),
         }
 
         encoded = json.dumps(packet).encode("utf-8")
